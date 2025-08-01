f8388842fd0803e7f4ae500689f25297a7107a05
nethuwh-xmd <nethumjayarathna8@gmail.com>
2025-08-01 19:41:54 +0530

    Delete aliveSchema.js

--- aliveSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const aliveSchema = new mongoose.Schema({
-  _id: { type: String, required: true }, // "aliveData"
-  imgUrl: { type: String },
-  videoUrl: { type: String }
-});
-
-module.exports = mongoose.model('AliveData', aliveSchema);
